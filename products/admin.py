f2c50c72fedc543417fd2d646f0307f85af25510
ahmed007dr <ahmed007eg@gmail.com>
2024-03-07 02:27:14 +0200

    add migre two class in same one product etc contnie

--- products/admin.py
+++ products/admin.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from .models import Products , ProductsImages , Review , Brand
 # Register your models here.
 
-class ProductImageInLine(admin.TabularInline):
+class ProductImageInline(admin.TabularInline):
     model = ProductsImages
 
-
 class ProductAdmin(admin.ModelAdmin):
-    inlines =[ProductsImages]
+    inlines = [ProductImageInline]
 
 # to merge to class in same one product with products image 
 
-admin.site.register(Products)
+admin.site.register(Products, ProductAdmin)
 admin.site.register(ProductsImages)
 admin.site.register(Review)
 admin.site.register(Brand)
