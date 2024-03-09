d0272b1bd166567219cea56116b0c48e91d855a1
ahmed007dr <ahmed007eg@gmail.com>
2024-03-09 18:24:57 +0200

    show product reviews from view not templates

--- products/views.py
+++ products/views.py
@@ -5,6 +5,18 @@ from .models import Products , Brand , Review
 
 class ProductList(ListView):
     model = Products
+    
 
 class ProductDetails(DetailView):
     model = Products
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+    
+
+
+ #context {'': } queryset : Product.objects.all()
+ #queryset : main query : detail product  | 
+ #context : extra data : review & image (extra) becouse we get new data from new table class
