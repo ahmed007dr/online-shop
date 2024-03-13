8a2ad8321f9eecf50626d46aa9a74287e53b4d78
ahmed007dr <ahmed007eg@gmail.com>
2024-03-13 03:28:52 +0200

    add app settings and finish urls , views , templates

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
+    path('', include("settings.urls")),
 
 ]
 
