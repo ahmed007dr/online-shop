6026ea3e19324aaf90dbc5800b616f617ef7d1f1
ahmed007dr <ahmed007eg@gmail.com>
2024-03-23 20:00:13 +0200

    add coupon

--- order/views.py
+++ order/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
-from .models import Order ,OrderDetail ,Cart ,CartDetail
+from django.shortcuts import get_object_or_404
+import datetime
+from .models import Order ,OrderDetail ,Cart ,CartDetail ,Coupon
 from products.models import Products
 from settings.models import DeliveryFee
 
@@ -11,9 +13,33 @@ def checkout(request):
     cart = Cart.objects.get(user=request.user,status='Inprogress')
     cart_detail = CartDetail.objects.filter(cart=cart)
     delivery_fee = DeliveryFee.objects.last().fee
-    sub_total = cart.cart_total
+
+    if request.method == 'POST':
+        code = request.POST['coupon_code']
+        #coupon = Coupon.objects.get(code=code)
+        coupon = get_object_or_404(Coupon,code=code)
+        if coupon and coupon.quantity > 0 :
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                coupon_value = round(cart.cart_total / 100  * coupon.discount,2)
+                sub_total = cart.cart_total - coupon_value
+                total = sub_total + delivery_fee
+                cart.coupon = coupon
+                cart.total_with_coupon = sub_total
+                cart.save()
+                return render(request, 'order/checkout.html', {
+                'cart_detail': cart_detail, 
+                'delivery_fee' : delivery_fee,
+                'sub_total':sub_total,
+                "discound": coupon_value,
+                'total': total
+                                })
+
+ 
+
+    sub_total = round(cart.cart_total,2)
     discound = 0
-    total = sub_total + delivery_fee
+    total = round(sub_total + delivery_fee,2)
     return render(request, 'order/checkout.html', {
         'cart_detail': cart_detail, 
         'delivery_fee' : delivery_fee,
