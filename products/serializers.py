02ec0321dba8750d1f2a8a8b950ffca2432a0d18
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 16:21:46 +0200

    add image API DETAILS PRO

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Products , Brand
+from .models import Products , Brand , ProductsImages , Review
+
+class ProductImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductsImages
+        fields = ['image']
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
@@ -31,7 +36,7 @@ class ProductDetailsSerializer(serializers.ModelSerializer):
 
     review_count = serializers.SerializerMethodField(method_name='get_review_count') # anther way
     avrg_rate = serializers.SerializerMethodField(method_name='get_avr_rate')
-
+    image = ProductImagesSerializer(source='product_image',many=True) # related name to filter one by one
     class Meta:
         model = Products
         fields = '__all__'
