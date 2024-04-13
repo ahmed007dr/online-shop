0c81b571079aadf64fe7d1aa6dc79c2c36659c3b
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 16:06:39 +0200

    add review count in list with related name and def

--- products/serializers.py
+++ products/serializers.py
@@ -3,9 +3,16 @@ from .models import Products , Brand
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+
+    review_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Products
         fields = '__all__'
+
+    def get_review_count(self,object):
+        reviews = object.reviews_product.all().count() #  (العلاقهreviews_product)كدا هنرجع بكل التقيمات و عددهم للمنتج الواحد
+        return reviews
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
