dddd24ab3862e1188e6bf3be043dbe5559ce541d
ahmed007dr <ahmed007eg@gmail.com>
2024-04-13 16:14:16 +0200

    avg rate in product details API

--- products/serializers.py
+++ products/serializers.py
@@ -30,6 +30,7 @@ class ProductDetailsSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
 
     review_count = serializers.SerializerMethodField(method_name='get_review_count') # anther way
+    avrg_rate = serializers.SerializerMethodField(method_name='get_avr_rate')
 
     class Meta:
         model = Products
@@ -39,6 +40,19 @@ class ProductDetailsSerializer(serializers.ModelSerializer):
     def get_review_count(self,object):
         reviews = object.reviews_product.all().count() #  (العلاقهreviews_product)كدا هنرجع بكل التقيمات و عددهم للمنتج الواحد
         return reviews
+    
+
+    def get_avr_rate(self,object): # عدد متوسط التقيمات
+        total = 0
+        reviews = object.reviews_product.all()
+        if len(reviews) > 0:
+            for item in reviews:
+                total += item.rate
+            
+            avg= total / len(reviews)
+        else :
+            avg =0
+        return avg
 
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
