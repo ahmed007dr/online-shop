8660ae7d4605e0c2051221e9de9159c7488a68fd
ahmed007dr <ahmed007eg@gmail.com>
2024-05-06 04:16:24 +0300

    skip redis

--- project/settings.py
+++ project/settings.py
@@ -178,12 +178,12 @@ INTERNAL_IPS = [  #Debug toolbar settings
 ]
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379",
+#     }
+# }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
