81c3bc3c4bc212097226f9f94bd949ced59b2d43
ahmed007dr <ahmed007eg@gmail.com>
2024-05-06 15:54:20 +0300

    settings for deploy

--- project/settings.py
+++ project/settings.py
@@ -77,6 +77,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  #newly added
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
