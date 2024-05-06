ed08c64781b0ee890c6b2f2880d9a86d5ece9bba
ahmed007dr <ahmed007eg@gmail.com>
2024-05-06 04:06:22 +0300

    add localhost

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-!0lrb@!xogu36)79b^8vu=(r)41%&-gce*l4em%@epm#st=c5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.226.145.213', 'online-shop-embabi.onrender.com', 'localhost']
+
 
 
 # Application definition
